ea9787640af0f5f62835c2d074fb6190753c5f3b
aqws000 <captaincushing@aol.com>
2018-10-14 23:02:48 -0400

    Taking Break. Working on implemtneting Google sign in button. Need to remember to set the intent for setup activity to Main activity. Need to test further

--- app/src/main/java/com/example/hayden/blogtest/LoginActivity.java
+++ app/src/main/java/com/example/hayden/blogtest/LoginActivity.java
@@ -3,19 +3,30 @@ package com.example.hayden.blogtest;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -29,10 +40,18 @@ public class LoginActivity extends AppCompatActivity {
     private Button mLoginBtn;
     private Button msignUp;
 
+    private static final String TAG = "LoginActivity";
+
     private FirebaseAuth mAuth;
+
+    private final static int RC_SIGN_IN = 1;
 
     private DatabaseReference mDatabaseUsers;
     private ProgressDialog mProgress;
+
+    private SignInButton mGoogleBtn;
+
+    private GoogleApiClient mGoogleApiClient;
 
 
 
@@ -45,6 +64,8 @@ public class LoginActivity extends AppCompatActivity {
         mDatabaseUsers = FirebaseDatabase.getInstance().getReference().child("Users");
         mDatabaseUsers.keepSynced(true);
         mProgress = new ProgressDialog(this);
+
+        mGoogleBtn = (SignInButton) findViewById(R.id.googleBtn);
 
         mLoginPasswordField = (EditText) findViewById(R.id.loginPasswordField); //Hayden these have issues
         mLoginEmailField = (EditText) findViewById(R.id.loginEmailField);
@@ -69,8 +90,93 @@ public class LoginActivity extends AppCompatActivity {
                 checkLogin();
             }
         });
+//--------------------------------------GOOGLE SIGN IN--------------------------------------------//
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
 
+        mGoogleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                signIn();
+
+            }
+        });
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+            .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                @Override
+                public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+
+                }
+            })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+        }
+
+
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+
+            mProgress.setMessage("Starting Sign In");
+            mProgress.show();
+
+           if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+               GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else{
+               mProgress.dismiss();
+               Toast.makeText(LoginActivity.this, "Error Login", Toast.LENGTH_LONG).show();
+
+
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        mProgress.show();
+
+                        // ...
+                    }
+                });
+    }
+
+
     private void checkLogin() {
 
         String email = mLoginEmailField.getText().toString().trim(); //Hayden these have issues
